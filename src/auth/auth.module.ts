98f7cb0f37d482d142951b19ab55f33d129f54b1
Alan <soldier4443@gmail.com>
2022-06-25 17:36:08 +0900

    Fix missing relative imports

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,9 @@
-import { Module } from '@nestjs/common';
-import { PassportModule } from '@nestjs/passport';
+import { AuthController } from '@/auth/auth.controller';
+import { AuthService } from '@/auth/auth.service';
 import { LocalStrategy } from '@/auth/local.strategy';
 import { UserModule } from '@/user/user.module';
-import { AuthService } from './auth.service';
-import { AuthController } from './auth.controller';
+import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
   imports: [UserModule, PassportModule],
