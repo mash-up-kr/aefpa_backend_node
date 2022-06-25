fee7b5cde6b2a1c8f52545bed8f0694bd1c788d6
Alan <soldier4443@gmail.com>
2022-06-25 21:06:04 +0900

    Add withPassword() function to User

--- src/user/entity/user.entity.ts
+++ src/user/entity/user.entity.ts
@@ -1,8 +1,22 @@
+import { TypesToOmit } from '@/util/types';
+
 // TODO: Add database connection
 export class User {
   id: number;
   email: string;
   password: string;
+
+  withoutPassword(): UserWithoutPassword {
+    return {
+      id: this.id,
+      email: this.email,
+    };
+  }
 }
 
-export type UserWithoutPassword = Omit<User, 'password'>;
+// 1. 함수가 아닌 프로퍼티만 뽑기 (Pick)
+// 2. password 제외하기 (Omit)
+export type UserWithoutPassword = Omit<
+  Pick<User, TypesToOmit<User, (...args: any) => any>>,
+  'password'
+>;
