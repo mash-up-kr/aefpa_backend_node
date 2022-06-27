45e130627383783cd261e89e1fd2dfdc4039846a
YeomJaeSeon <a89541457@gmail.com>
2022-06-28 00:24:38 +0900

    prisma setting fix & vscode debug add

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,5 +1,3 @@
-import { Prisma, PrismaClient, User } from '@/api/server/generated';
-import { PrismaService } from '@/prisma/prisma.service';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { AppModule } from '@/app.module';
+import { PrismaService } from '@/prisma/prisma.service';
 import { NestFactory } from '@nestjs/core';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const prismaService = app.get(PrismaService);
+  prismaService.enableShutdownHooks(app);
+
   await app.listen(3000);
 }
 bootstrap();
