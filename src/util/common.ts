f1b026330125cfaa68b7cc2738aa760d3342e52d
Alan <soldier4443@gmail.com>
2022-07-06 22:47:52 +0900

    Add a comment in utility function for clarity

--- src/util/common.ts
+++ src/util/common.ts
@@ -1,3 +1,7 @@
+/**
+ * If value is not null, apply the mapper and return the result.
+ * If value is null, return the default value.
+ */
 export function runOrDefault<T, U>(
   value: T | null | undefined,
   mapper: (value: T) => U,
