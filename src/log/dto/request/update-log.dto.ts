06bad40b9e23576b3fce7ce8ac0f671b89cc25d5
YeomJaeSeon <a89541457@gmail.com>
2022-08-20 15:18:16 +0900

    간딴 끼록 수정 api 수정

--- src/log/dto/request/update-log.dto.ts
+++ src/log/dto/request/update-log.dto.ts
@@ -1,10 +1,19 @@
 import { LogDto } from '@/log/dto/log.dto';
-import { OmitType } from '@nestjs/swagger';
+import { ApiProperty, OmitType } from '@nestjs/swagger';
 
 export class UpdateLogDto extends OmitType(LogDto, [
   'id',
   'createdAt',
   'updatedAt',
+  'cursor',
   'like',
   'isScrapped',
-]) {}
+  'images',
+]) {
+  @ApiProperty({
+    type: 'array',
+    items: { type: 'string', format: 'binary' },
+    multipleOf: 1,
+  })
+  images: any;
+}
