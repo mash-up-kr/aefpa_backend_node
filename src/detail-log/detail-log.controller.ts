8e08bbd9f220e1c3aed0823adce3f95ff3333540
YeomJaeSeon <a89541457@gmail.com>
2022-08-27 14:33:34 +0900

    페이지네이션 없이도 전체 조회가능하게 validation수정

--- src/detail-log/detail-log.controller.ts
+++ src/detail-log/detail-log.controller.ts
@@ -99,6 +99,9 @@ export class DetailLogController {
     @Query() cursorPaginationRequestDto: CursorPaginationRequestDto,
     @User() user: UserWithoutPassword,
   ) {
+    if (cursorPaginationRequestDto.endCursor && !cursorPaginationRequestDto.pageSize) {
+      throw new BadRequestException('endCursor는 있는데 pageSize가 없을수 없어요');
+    }
     return await this.detailLogService.findAll(cursorPaginationRequestDto, user);
   }
 
